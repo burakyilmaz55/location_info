c84c54e3f2e872feb7f2434f4566dfbed8dfba78
burakyilmaz55 <burakyilmz17@gmail.com>
2020-09-17 16:43:52 +0300

    update

---
+++ main.py
@@ -0,0 +1,23 @@
+import urllib.request
+import urllib
+import socket
+import geocoder
+
+
+url = input("Website url: ")
+
+ip2 = url.replace('http://', '')
+ip = socket.gethostbyname(ip2)
+g = geocoder.ip(ip)
+if True:
+
+    print(
+      f"Websiteniz çalışır durumdadır\n"
+      f"IP: {ip}\n"
+      f"Lokasyon: {g.latlng}\n"
+      f"Şehir: {g.city}\n"
+      f"Posta Kodu: {g.postal}\n"
+      f"Adres: {g.address} "
+      )
+else:
+    print("Websiteniz çalşır durumda değildir.")
